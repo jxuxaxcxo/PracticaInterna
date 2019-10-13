487b3dcbd0130b7ade5ff3701e456f7608b6d231
javivi235 <javisoruco235@gmail.com>
2019-10-13 16:26:10 -0400

    vuetify added to project

--- src/router.js
+++ src/router.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import FuenteNCView from './views/FuenteNCView'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    //{
-    // path: '/',
-    //  name: 'home',
-    //  component: Home
-    // },
+    {
+    path: '/',
+     name: 'home',
+     component: Home
+    },
+
+    {
+      path: '/nuevaFuente/',
+      name: 'nuevaFuente',
+      component: FuenteNCView
+    }
 
   ]
 })
---
+++ src/views/FuenteNCView.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <h6>HOLAA</h6>
+
+</template>
+
+<script>
+export default {
+    name: 'FuenteNCView'
+}
+</script>
+
+<style scoped>
+
+</style>
