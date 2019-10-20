b3068f966123f5f14d01407ce1ded6845d7c8566
javivi235 <javisoruco235@gmail.com>
2019-10-20 15:15:34 -0400

    interfaz de confimacion y cambio de color

---
+++ src/components/FuenteNC/ConfirmacionForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="contenedor">
+        <v-card>
+            <v-card-title>{{titulo}}</v-card-title>
+            <v-card-text>{{texto}}</v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="#FFC30F" class="white--text" @click="confirmar">Confirmar</v-btn>
+                <v-btn color="#3B83BD" class="white--text" @click="cancelar">Cancelar</v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ConfirmacionForm',
+    props: {
+        titulo: String,
+        texto: String,
+    },
+    methods: {
+        confirmar () {
+
+        },
+        cancelar () {
+            this.$emit("cancelarFuente");
+        }
+    }
+}
+</script>
+<style scoped>
+.contenedor {
+     width: 80%;
+     margin-left: 10%;
+ }
+</style>
--- src/components/FuenteNC/NCForm.vue
+++ src/components/FuenteNC/NCForm.vue
@@ -3,26 +3,65 @@
         <v-text-field
             label="Titulo No-Conformidad"
             id ="tituloNCF"
+            :v-model="tituloNCF"
+            clearable
           ></v-text-field>
         <v-text-field
             label="Descripcion"
-            id="DescripcionNCF"
+            id="descripcionNCF"
+            :v-model="descripcionNCF"
+            clearable
         >
         </v-text-field>
-        <v-combobox
+        <v-overflow-btn
         label="Seleccione un Encargado"
-        chips
-        autofocus
-        type="button"
-        class="seleccion"
-        id="EncargadoNCF"
+        id="encargadoNCF"
+        :v-model="encargadoNCF"
         >
-        </v-combobox>
+        </v-overflow-btn>
+        <v-container>
+            <v-row>
+                <v-col cols="6"></v-col>
+                <v-col cols="3">
+                    <v-btn
+                    color="#FFC30F"
+                    class="center white--text"
+                    id="confirmarNCF"
+                    @click="confirmar"
+                    >
+                        Confirmar
+                    </v-btn>
+                </v-col>
+                <v-col cols="3">
+                    <v-btn
+                    color="#3B83BD"
+                    class="center white--text"
+                    id="confirmarNCF"
+                    @click="cancelar"
+                    >
+                        Cancelar
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 <script>
 export default {
     name: 'NCForm',
+    props: {
+        tituloNCF: String,
+        descripcionNCF: String,
+        encargadoNCF: String
+    },
+    methods: {
+        confirmar(){
+            this.$emit("confirmarDatosNCF", tituloNCF, descripcionNCF, encargadoNCF);
+        },
+        cancelar(){
+            this.$emit("cancelar");
+        }
+    },
 }
 </script>
 <style scoped>
