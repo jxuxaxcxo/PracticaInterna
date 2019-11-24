efc6edb1aec59ba6b35964c2556f2ec47ee571cf
Mateo <mateopuna98@gmail.com>
2019-11-24 18:57:29 -0400

    RF-4 : Creada ventana de Login

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <v-app>
-    <v-content>
-    <div>
-      <Navbar/>
-    </div>
-    <div>
-
-    <router-view></router-view>
-    </div>
-    </v-content>
+<Login></Login>
   </v-app>
 </template>
 
 <script>
 import Navbar from './views/Navbar.vue';
 import ListaPer from './views/ListaPer.vue';
+import Login from './views/LoginView.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Login
   },
   data: () => ({
     //
---
+++ src/views/LoginView.vue
@@ -0,0 +1,90 @@
+<template>
+<div id = "app" class = "back">
+ <v-app id="inspire"  >
+
+    <v-content>
+
+      <v-container
+        class="fill-height"
+        fluid
+      >
+
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+
+              <v-toolbar
+                color="#003366"
+                dark
+                flat
+              >
+                <v-toolbar-title>Sistema de Gestión de Calidad UPB</v-toolbar-title>
+                <v-spacer />
+                <v-tooltip bottom>
+                  
+                </v-tooltip>
+                <v-tooltip right>
+                 
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+
+                <v-form>
+
+                  <v-text-field
+                    id = "login"
+                    label="Usuario"
+                    name="login"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Contraseña"
+                    name="password"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="#003366" dark>Entrar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+
+</div>
+ 
+</template>
+
+<script>
+  export default {
+
+    name : 'Login',
+    props: {
+      source: String,
+    },
+  }
+</script>
+
+<style scoped>
+
+.back {
+  background-image: url(https://i0.wp.com/gc.edu/wp-content/uploads/2016/03/iStock_76338075_MEDIUM.jpg?ssl=1);
+  background-size: cover;
+}
+#inspire {
+  background: none;
+}
+</style>
