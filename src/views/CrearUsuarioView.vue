9f321fab93267426a93702f48bc09e3887defb04
Mateo <mateopuna98@gmail.com>
2019-12-09 00:33:19 -0400

    popup y clickt abla creado

--- src/views/CrearUsuarioView.vue
+++ src/views/CrearUsuarioView.vue
@@ -91,7 +91,7 @@
                 </v-layout>
                 <v-layout row>
                   <v-flex xs12>
-                    <v-btn type="submit" color="#003366" dark>Sign up</v-btn>
+                    <v-btn type="submit" color="#003366" dark>Crear Usuario</v-btn>
                   </v-flex>
                 </v-layout>
               </form>
@@ -107,7 +107,120 @@
     :items="desserts"
     :items-per-page="5"
     class="elevation-1"
+    @click:row = "openEditWindow"
+
   ></v-data-table>
+
+
+    <v-dialog
+          v-model="dialog"
+          max-width="100%"
+        >
+
+        <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-title>Crear usuario</v-card-title>
+          
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent="clickRegistrar">
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="nombre"
+                      label="Nombre"
+                      id="nombre"
+                      v-model="nombreDialog"
+                      type="text"
+                      color="#003366"
+                      required>
+                      </v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                 <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="apellido"
+                      label="Apellido"
+                      id="apellido"
+                      v-model="apellidoDialog"
+                      type="text"
+                      color="#003366"
+                      required>
+                      </v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                  <v-flex xs12>
+                  <v-col class="d-flex" cols="12" sm="6">
+                    <v-select
+                      :items="cargos"
+                      v-model="cargoDialog"
+                      label="Cargo"
+                    ></v-select>
+                  </v-col>
+                  </v-flex>
+                </v-layout>
+
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Correo electrónico"
+                      id="email"
+                      v-model="emailDialog"
+                      type="email"
+                      color="#003366"
+                      required>
+                      </v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Contraseña"
+                      id="password"
+                      v-model="contrasenaDialog"
+                      type="password"
+                      color="#003366"
+                      required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="confirmarContrasena"
+                      label="Confirmar contraseña"
+                      id="confirmarContrasena"
+                      v-model="confirmarContrasenaDialog"
+                      type="password"
+                      color="#003366"
+                      :rules="[compararContrasenas]"></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit" color="#003366" dark>Editar Usuario</v-btn>
+                     <v-btn  color="#B11226" dark>Eliminar Usuario</v-btn>
+
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+     
+    </v-dialog>
 
 </div>
  
@@ -118,6 +231,15 @@
     name : 'CrearUsuario',
     data () {
       return {
+
+        dialog : false,
+
+        emailDialog: '',
+        contrasenaDialog: '',
+        nombreDialog: '',
+        apellidoDialog: '',
+        cargoDialog: '',
+        confirmarContrasenaDialog: '',
 
         
         email: '',
@@ -218,19 +340,18 @@
  
     methods: {
       clickRegistrar () {
-        console.log(this.nombre)
-        console.log(this.apellido)
-        console.log(this.email)
-        console.log(this.contrasena)
-        console.log(this.cargo)
-
+ 
         this.$store.dispatch('registrarUsuario', {nombre: this.nombre, apellido: this.apellido,
                                                   email: this.email, contrasena: this.contrasena,
                                                   cargo: this.cargo})
       },
 
       compararContrasenas(){
-        return this.confirmarContrasena === this.confirmarContrasena
+        return this.confirmarContrasena === this.contrasena
+      },
+
+      openEditWindow(){
+        this.dialog = true
       }
     }
   }
